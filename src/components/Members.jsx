5b90d6320496739421053754e6023595d60b364e
janetshinyv <janetshinyv.cse2022@citchennai.net>
2024-06-04 08:47:51 +0530

    added responsiveness to Review.jsx

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 export default function Members() {
   return (
     <div className='pb-32'>
-        <div className='text-center font-arial text-7xl font-bold text-white pb-28'>Welcome our team members</div>
-        <div className='flex-col space-y-28'>
-            <div className='flex justify-around '>
-             <p className='h-40 w-40 bg-white rounded-full'></p>
-             <p className='h-40 w-40 bg-white rounded-full'></p>
-             <p className='h-40 w-40 bg-white rounded-full'></p>
-             <p className='h-40 w-40 bg-white rounded-full'></p> 
-            </div>
-            <div className='flex justify-evenly'>
-            <p className='h-40 w-40 bg-white rounded-full'></p>
-            <p className='h-40 w-40 bg-white rounded-full'></p>
-            <p className='h-40 w-40 bg-white rounded-full'></p>
-
-            </div>
-
+      <div className='text-center font-arial text-4xl md:text-7xl font-bold text-white pb-16 md:pb-28'>
+        Welcome our team members
+      </div>
+      <div className='space-y-16 md:space-y-28'>
+        <div className='flex flex-wrap justify-center gap-8 md:gap-32'>
+          <p className='h-32 w-32 md:h-40 md:w-40 bg-white rounded-full'></p>
+          <p className='h-32 w-32 md:h-40 md:w-40 bg-white rounded-full'></p>
+          <p className='h-32 w-32 md:h-40 md:w-40 bg-white rounded-full'></p>
+          <p className='h-32 w-32 md:h-40 md:w-40 bg-white rounded-full'></p>
         </div>
-
+        <div className='flex flex-wrap justify-center gap-8 md:gap-36'>
+          <p className='h-32 w-32 md:h-40 md:w-40 bg-white rounded-full'></p>
+          <p className='h-32 w-32 md:h-40 md:w-40 bg-white rounded-full'></p>
+          <p className='h-32 w-32 md:h-40 md:w-40 bg-white rounded-full'></p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
