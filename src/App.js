8366881472a171776293099c51e440020e13d571
Abishek-7ak <abishekcsecs@gmail.com>
2024-08-17 15:56:28 +0530

    Unnecessary codes are cleaned

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
-import About from './components/About';
-import Home from './components/Home';
 import Main from './components/main';
-import Navigation from './components/navigation';
-
-
 
 function App() {
   return (
     <div className="App font-roboto">
-      
       <Main />
-    
     </div>
   );
 
